2938a9f9da41eeca4a4a9a5aa3153f01597307c3
cai zhibiao <cai_zhibiao@163.com>
2017-12-08 14:57:47 +0800

    Initial commit

---
+++ libtwo/src/test/java/com/libtwo/libtwoUnitTest2.java
@@ -0,0 +1,17 @@
+package com.libtwo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class libtwoUnitTest2 {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
